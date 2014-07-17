35f7bce9b9d172b8028d5ee699f0aa86cc325e76
Aaron Wells <chacewells@gmail.com>
2014-07-17 16:37:39 -0700

    Initial Commit

---
+++ WorkWell/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WorkWell
+//
+//  Created by Aaron Wells on 7/17/14.
+//  Copyright (c) 2014 Aaron Wells. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ WorkWellTests/WorkWellTests.m
@@ -0,0 +1,34 @@
+//
+//  WorkWellTests.m
+//  WorkWellTests
+//
+//  Created by Aaron Wells on 7/17/14.
+//  Copyright (c) 2014 Aaron Wells. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WorkWellTests : XCTestCase
+
+@end
+
+@implementation WorkWellTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
